8db249a94e342273f602b8bbfff18625ba9ff69b
Ana Lydia Moreira <analydiamoreira965@gmail.com>
2026-03-23 21:57:53 -0300

    feat: add Vault level contracts

---
+++ test/vault/VaultTest.t.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "src/vault/Vault.sol";
+
+contract VaultTest is Test {
+
+    address deployer;
+    address attacker;
+    Vault vault;
+
+    function setUp() public {
+        deployer = makeAddr("deployer");
+        attacker = makeAddr("attacker");
+
+        vm.prank(deployer);
+        vault = new Vault(bytes32(uint256(12345)));
+    }
+
+    function test_attack() public {
+        vm.prank(attacker);
+        vm.load(address(vault), bytes32(uint256(1)));
+        assertTrue(vault.locked(), "false");
+
+    }
+}
